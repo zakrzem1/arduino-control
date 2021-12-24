a49ef8b284d58bd01da245781c1852bb9124c848
zakrzem1 <maciek.zakrzewski@gmail.com>
2021-12-24 11:27:28 +0100

    fixed obvious syntax errors

--- mza-mega-ssr-relay-wallswitch/send_firmata.py
+++ mza-mega-ssr-relay-wallswitch/send_firmata.py
@@ -1,10 +1,12 @@
-from pyfirmata import Arduino, util
+from pyfirmata import Arduino, util, STRING_DATA
 board = Arduino('/dev/ttyUSB0')
-fun handle_string(received):
-	print('received:')
-	println(received)
-board.add_cmd_handler(STRING_DATA, handle_string())
-println('sending')
+def handle_string(*received):
+	print('received:') # TODO chunk in 2 bytes chunks
+	print(util.from_two_bytes(received))
+board.add_cmd_handler(STRING_DATA, handle_string)
+print('sending')
 board.send_sysex(0x71, util.str_to_two_byte_iter("maciek\0"))
-while(True)
-	board.iterate()
+
+while(True):
+    while board.bytes_available():
+        board.iterate()

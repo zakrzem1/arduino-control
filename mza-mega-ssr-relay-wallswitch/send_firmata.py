49d40429f3e5c8769ace5dae010fd9cf5778c9f7
zakrzem1 <maciej.zakrzewski@vimn.com>
2021-12-27 07:12:47 +0100

    python add proper parsing of incoming strings, expose a bottle http endpoints for operating:  http POST localhost:8080/gate/GC, http POST localhost:8080/gate/GO

--- mza-mega-ssr-relay-wallswitch/send_firmata.py
+++ mza-mega-ssr-relay-wallswitch/send_firmata.py
@@ -1,12 +1,41 @@
 from pyfirmata import Arduino, util, STRING_DATA
-board = Arduino('/dev/ttyUSB0')
+from bottle import get, post, run, template
+lastReceived='nothing'
+gate_ops = ('GO','GC')
 def handle_string(*received):
-	print('received:') # TODO chunk in 2 bytes chunks
-	print(util.from_two_bytes(received))
-board.add_cmd_handler(STRING_DATA, handle_string)
-print('sending')
-board.send_sysex(0x71, util.str_to_two_byte_iter("maciek\0"))
+    global lastReceived
+    print('received:')
+    lastReceived="".join(map(chr, map(util.from_two_bytes,zip(received[0::2],received[1::2]))))
+    print(lastReceived)
 
-while(True):
-    while board.bytes_available():
-        board.iterate()
+# '/dev/ttyUSB0' on rpi
+board = Arduino('/dev/tty.usbserial-14230')
+board.add_cmd_handler(STRING_DATA, handle_string)
+
+def send_string(msg):
+    print('sending')
+    board.send_sysex(0x71, util.str_to_two_byte_iter(msg+'\0'))
+
+it = util.Iterator(board)
+it.start()
+
+@get('/last')
+def index():
+    global lastReceived
+    return template('Last message: <b>{{lastMsg}}</b>!', lastMsg=lastReceived)
+
+
+@post('/gate/<op>')
+def gate(op):
+    print("gate: "+ op)
+    if(op in gate_ops):
+        send_string(op)
+    else:
+        print('unknown op: '+op)
+    return template('<b>Operation {{op}}</b>!', op=op)
+
+try:
+    run(host='localhost', port=8080)
+except:
+    board.exit()
+    print('bye!')
